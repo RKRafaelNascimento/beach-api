bd4b5159332391340eff402d065193391c3d5909
RKRafaelNascimento <RKRafael.nascimento@gmail.com>
2020-08-15 22:44:37 -0300

    Configurando mongoDB

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,17 @@ import { Server as OvernightServer } from '@overnightjs/core';
 import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/Forecast';
 import { Application } from 'express';
+import * as database from '@src/utils/database';
 
 export class Server extends OvernightServer {
   constructor(private port = 3000) {
     super();
   }
 
-  public init(): void {
+  public async init(): Promise<void> {
     this.setupExpress();
     this.setupControllers();
+    await this.databaseSetup();
   }
 
   private setupExpress(): void {
@@ -21,6 +23,14 @@ export class Server extends OvernightServer {
   private setupControllers(): void {
     const forecastController = new ForecastController();
     this.addControllers([forecastController]);
+  }
+
+  private async databaseSetup(): Promise<void> {
+    await database.connect();
+  }
+
+  public async close(): Promise<void> {
+    await database.close();
   }
 
   public getApp(): Application {
