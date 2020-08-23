8b81ebbbc5c99c4e1e821ba54f12bdf0b9f98317
RKRafaelNascimento <RKRafael.nascimento@gmail.com>
2020-08-22 22:51:58 -0300

    Módulo de Users - Mongoose hooks, validation e Type narrowing

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -15,6 +15,35 @@ describe('Users functional tests', () => {
       const response = await global.testRequest.post('/user').send(newUser);
       expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(newUser));
-    });
+    }),
+      it('should return 422 when there is a validation error', async () => {
+        const newUser = {
+          email: 'john@teste2.com',
+          password: '123456',
+        };
+
+        const response = await global.testRequest.post('/user').send(newUser);
+        expect(response.status).toBe(422);
+        expect(response.body).toEqual({
+          code: 422,
+          error: 'User validation failed: name: Path `name` is required.',
+        });
+      }),
+      it('should return 409 when the email already exists', async () => {
+        const newUser = {
+          name: 'John',
+          email: 'john@teste.com',
+          password: '123456',
+        };
+
+        const response = await global.testRequest.post('/user').send(newUser);
+
+        expect(response.status).toBe(409);
+        expect(response.body).toEqual({
+          code: 409,
+          error:
+            'User validation failed: email: already exists in the database',
+        });
+      });
   });
 });
