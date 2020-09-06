eeb97c487a362c651a04d7bd32e3550e22b8189c
RKRafaelNascimento <RKRafael.nascimento@gmail.com>
2020-09-05 23:09:09 -0300

    Adiciona endpoint para decoded token

--- src/controllers/Users.ts
+++ src/controllers/Users.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from 'express';
-import { Post, Controller } from '@overnightjs/core';
+import { Post, Controller, Get, Middleware } from '@overnightjs/core';
 import { User } from '@src/models/user';
 import { BaseController } from '.';
 import AuthService from '@src/services/auth';
+import { authMiddleware } from '@src/middlewares/auth';
 
 @Controller('user')
 export class UserController extends BaseController {
@@ -40,5 +41,20 @@ export class UserController extends BaseController {
 
     const token = AuthService.generatedToken(user.toJSON());
     return res.status(200).send({ token: token });
+  }
+
+  @Get('me')
+  @Middleware(authMiddleware)
+  public async me(req: Request, res: Response): Promise<Response> {
+    const email = req.decoded ? req.decoded.email : undefined;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return this.sendErrorResponse(res, {
+        code: 404,
+        message: 'User not found!',
+      });
+    }
+
+    return res.send({ user });
   }
 }
