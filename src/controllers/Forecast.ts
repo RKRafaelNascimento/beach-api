3b62c6e05c58f942baeeaa18956a7822f7914e78
RKRafaelNascimento <RKRafael.nascimento@gmail.com>
2020-09-12 22:22:23 -0300

    Rate limiting em Node.js com express e finalização

--- src/controllers/Forecast.ts
+++ src/controllers/Forecast.ts
@@ -1,17 +1,41 @@
-import { Controller, Get, ClassMiddleware } from '@overnightjs/core';
+import {
+  Controller,
+  Get,
+  ClassMiddleware,
+  Middleware,
+} from '@overnightjs/core';
 import { Request, Response } from 'express';
 import { Forecast } from '@src/services/forecast';
 import { Beach } from '@src/models/beach';
 import { authMiddleware } from '@src/middlewares/auth';
 import logger from '@src/logger';
 import { BaseController } from '.';
+import ApiError from '@src/utils/errors/api.error';
+import rateLimit from 'express-rate-limit';
 
 const forecast = new Forecast();
+
+const rateLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute in milliseconds
+  max: 10,
+  keyGenerator(req: Request): string {
+    return req.ip;
+  },
+  handler(_, res: Response): void {
+    res.status(429).send(
+      ApiError.format({
+        code: 429,
+        message: "Too many requests to the '/forecast endpoint'",
+      })
+    );
+  },
+});
 
 @Controller('forecast')
 @ClassMiddleware(authMiddleware)
 export class ForecastController extends BaseController {
   @Get('')
+  @Middleware(rateLimiter)
   public async getForecastForLoggedUser(
     req: Request,
     res: Response
